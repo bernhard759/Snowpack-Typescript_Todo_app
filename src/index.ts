8c061a8981db937339a9d28e524cb9b7b2ac5481
bernhard759 <bernhard.gailer@gmx.de>
2022-12-28 13:28:53 +0100

    add more functions and more styles

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
-// es6 module import
+/* Import */
 import { v4 as uuidV4 } from "uuid"
+import { drawSmiley, drawBubble, drawText, drawStickMan, drawHeart } from "./drawings";
+import './style.css';
 
-// declare task type
-type Task = { id: string, title: string, completed: boolean, createdAt: Date }
 
+/* Task type */
+type Task = { id: string, title: string, completed: boolean, createdAt: Date };
 
-// get the html elements
-const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.getElementById("new-task-form") as HTMLFormElement | null
-const input = document.querySelector<HTMLInputElement>("#new-task-title")
+/* HTML ELements */
+const list = document.querySelector<HTMLUListElement>("#list");
+const addBtn = document.querySelector<HTMLButtonElement>("div.input-group button");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
-// Task array
+/* Task array */
 const tasks: Task[] = loadTasks()
 
-// render tasks to page
+/* Render tasks to the page */
 tasks.forEach(addListItem)
 
-// Eventlistener on form
-form?.addEventListener("submit", e => {
 
-  // prevent default event
-  e.preventDefault()
+/* Add Eventlistener to button */
+addBtn?.addEventListener("click", e => {
 
-  // get the input value
-  if (input?.value == "" || input?.value == null) return
+  /* Input value */
+  if (input?.value == "" || input?.value == null) {
+    console.warn("cant create todo without title")
+    return
+  }
 
-  // todo task
+  /* Create new todo task object */
   const newTask: Task = {
     id: uuidV4(),
     title: input.value,
@@ -33,46 +36,82 @@ form?.addEventListener("submit", e => {
     createdAt: new Date()
   }
 
-  // push task to task array
+  /* Push task to array */
   tasks.push(newTask)
   saveTasks(tasks)
 
-  // add item to list
+  /* Add item to list */
   addListItem(newTask)
-  console.log("added item")
+  console.log("added task ", newTask.title)
 
-  // clear input
+  /* Clear the input value */
   input.value = ""
 
 })
 
+/** Add a list item to the DOM and save it to local storage */
 function addListItem(task: Task) {
 
-  // create html elements
-  const item = document.createElement("li")
-  const label = document.createElement("label")
-  const checkbox = document.createElement("input")
-  checkbox.type = "checkbox"
-  checkbox.checked = task.completed
-  label.append(checkbox, task.title)
-  item.append(label)
-  list?.append(item)
+  /* Create HTML Elements */
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  const closeSpan = document.createElement("span");
+  const closeIcon = document.createElement("i");
+  /* Change attributes */
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+  closeIcon.classList.add("fa", "fa-times");
+  /* Append to the DOM */
+  closeSpan.append(closeIcon);
+  label.append(checkbox, task.title);
+  item.append(label);
+  item.append(closeSpan);
+  list?.append(item);
 
-  // Eventlistener
+  /* Checkbox Eventlistener */
   checkbox.addEventListener("change", () => {
-    task.completed = checkbox.checked
-    saveTasks(tasks)
-    console.log(tasks)
+    task.completed = checkbox.checked; // mark as completed
+    checkbox.closest("li")?.classList.toggle("done") // toggle done class
+    saveTasks(tasks); // save to local storage
+    checkbox.closest("li")?.classList.contains("done") ?
+      console.log("marked task ", task.title, " as completed") :
+      console.log("marked task ", task.title, " as todo");
+    console.log("tasks ", tasks);
+  })
+
+  /* Close Span Eventlistener */
+  closeSpan.addEventListener("click", () => {
+    closeSpan.closest("li")?.remove(); // remove from DOM
+    const thisTask = tasks.filter(x => x.id == task.id)[0]; // filter tasks array
+    tasks.splice(tasks.indexOf(thisTask), 1); // remove this task
+    saveTasks(tasks); // save to local storage
+    console.log("removed task ", thisTask.title);
+    console.log("tasks ", tasks);
   })
 
 }
 
+/** Save stringified tasks array to local storage */
 function saveTasks(tasks: Task[]) {
   localStorage.setItem("TASKS", JSON.stringify(tasks))
 }
 
+/** Return parsed tasks array from local storage */
 function loadTasks(): Task[] {
   const taskJSON = localStorage.getItem("TASKS")
   if (taskJSON == null) return []
   return JSON.parse(taskJSON)
 }
+
+/* Canvas Drawings */
+const canvasSmiley = document.querySelector<HTMLCanvasElement>("canvas.smiley");
+if (canvasSmiley) drawSmiley(canvasSmiley);
+const canvasBubble = document.querySelector<HTMLCanvasElement>("canvas.bubble");
+if (canvasBubble) drawBubble(canvasBubble);
+const canvasText = document.querySelector<HTMLCanvasElement>("canvas.text");
+if (canvasText) drawText(canvasText);
+const canvasStickMan = document.querySelector<HTMLCanvasElement>("canvas.stickman");
+if (canvasStickMan) drawStickMan(canvasStickMan);
+const canvasHeart = document.querySelector<HTMLCanvasElement>("canvas.heart");
+if (canvasHeart) drawHeart(canvasHeart);
